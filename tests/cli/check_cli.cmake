4b9ca854ab8663e24353cb7affab03d87228c2f5
anothel <anothel1@naver.com>
2026-06-14 20:49:58 +0900

    test: strengthen CLI failure checks

--- tests/cli/check_cli.cmake
+++ tests/cli/check_cli.cmake
@@ -18,6 +18,24 @@ function(run_cli expected_exit expected_stdout)
   endif()
 endfunction()
 
+function(run_cli_failure)
+  execute_process(
+    COMMAND "${SECUREKIT_CLI}" ${ARGN}
+    RESULT_VARIABLE result
+    OUTPUT_VARIABLE stdout
+    ERROR_VARIABLE stderr)
+
+  if(result EQUAL 0)
+    message(FATAL_ERROR "Expected command to fail: ${ARGN}")
+  endif()
+  if(NOT stdout STREQUAL "")
+    message(FATAL_ERROR "Failed command should not write stdout. stdout=[${stdout}]")
+  endif()
+  if(stderr STREQUAL "")
+    message(FATAL_ERROR "Failed command should write stderr")
+  endif()
+endfunction()
+
 run_cli(0 "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad\n" sha256 --text abc)
 run_cli(0 "616263\n" hex-encode --text abc)
 run_cli(0 "abc\n" hex-decode --text 616263)
@@ -42,32 +60,7 @@ if(NOT token_length EQUAL 22 OR token_text MATCHES "[^A-Za-z0-9_-]")
   message(FATAL_ERROR "token output is not 22-char unpadded Base64URL text: [${token_stdout}]")
 endif()
 
-execute_process(
-  COMMAND "${SECUREKIT_CLI}" token 0
-  RESULT_VARIABLE bad_result
-  OUTPUT_VARIABLE bad_stdout
-  ERROR_VARIABLE bad_stderr)
-if(bad_result EQUAL 0)
-  message(FATAL_ERROR "token 0 should fail")
-endif()
-if(NOT bad_stdout STREQUAL "")
-  message(FATAL_ERROR "invalid usage should not write stdout")
-endif()
-if(bad_stderr STREQUAL "")
-  message(FATAL_ERROR "invalid usage should write stderr")
-endif()
-
-execute_process(
-  COMMAND "${SECUREKIT_CLI}" hex-decode --text 6
-  RESULT_VARIABLE bad_hex_result
-  OUTPUT_VARIABLE bad_hex_stdout
-  ERROR_VARIABLE bad_hex_stderr)
-if(bad_hex_result EQUAL 0)
-  message(FATAL_ERROR "invalid hex should fail")
-endif()
-if(NOT bad_hex_stdout STREQUAL "")
-  message(FATAL_ERROR "invalid hex should not write stdout")
-endif()
-if(bad_hex_stderr STREQUAL "")
-  message(FATAL_ERROR "invalid hex should write stderr")
-endif()
+run_cli_failure(token 0)
+run_cli_failure(sha256 --file "${CMAKE_CURRENT_BINARY_DIR}/securekit-cli-missing.txt")
+run_cli_failure(hex-decode --text 6)
+run_cli_failure(base64url-decode --text "=")
