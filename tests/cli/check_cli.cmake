39b45cb0d2808f9210321433833e83a7a7a9956e
anothel <anothel1@naver.com>
2026-06-14 20:51:42 +0900

    test: cover CLI help output

--- tests/cli/check_cli.cmake
+++ tests/cli/check_cli.cmake
@@ -36,6 +36,19 @@ function(run_cli_failure)
   endif()
 endfunction()
 
+set(expected_help [=[Usage:
+  securekit token <byte-size>
+  securekit sha256 --text <text>
+  securekit sha256 --file <path>
+  securekit hex-encode --text <text>
+  securekit hex-decode --text <hex>
+  securekit base64url-encode --text <text>
+  securekit base64url-decode --text <base64url>
+  securekit help
+]=])
+
+run_cli(0 "${expected_help}" help)
+run_cli(0 "${expected_help}" --help)
 run_cli(0 "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad\n" sha256 --text abc)
 run_cli(0 "616263\n" hex-encode --text abc)
 run_cli(0 "abc\n" hex-decode --text 616263)
